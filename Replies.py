a5686c81b9dd2a21fb2410b3b71265d81e55ebc0
GoblinHater <70269209+GoblinHater@users.noreply.github.com>
2020-10-09 13:33:11 +0530

    Template of reply strings
    Template of reply strings

---
+++ Replies.py
@@ -0,0 +1,6 @@
+source = "^[Source](https://github.com/GoblinHater/sauceNAOcheckbot)"
+FAQ = "^[FAQ](https://www.reddit.com/r/sauceNAOcheckbot/comments/iocsj9/saucenaocheckbot_faq/)"
+botphrse = "^I ^am ^a ^bot. ^give ^feedback ^to u/GoblinHater. ^Wrong ^sauce? ^reply ^with ^wrong."
+footer = botphrse + " || " + source + " || " + FAQ
+Negative_reply = "Sorry I was not able to find the sauce.\n\nI did not check the low similarity options\n\n"
+Wrong_file = "Specified file is of a wrong format. sauceNAO accepts images of format .jpg, .jpeg, .png or .gif \n\n"
