bdf9eb283dbea29477de1724c152e71df5a0ff5d
GoblinHater <70269209+GoblinHater@users.noreply.github.com>
2020-10-09 13:56:50 +0530

    Now using bs4 and requests

--- SauceNAO.py
+++ SauceNAO.py
@@ -1,128 +1,107 @@
-import selenium
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver import ActionChains
-import time
+import requests
+from bs4 import BeautifulSoup
+from Replies import *
 
-# PATH = "C:\Program Files (x86)\chromedriver.exe"
-# driver = webdriver.Chrome(PATH)
+# Below are different test cases I used:-
 
-source = "[Source](https://github.com/GoblinHater/sauceNAOcheckbot)"
-FAQ = "[FAQ](https://www.reddit.com/r/sauceNAOcheckbot/comments/iocsj9/saucenaocheckbot_faq/)"
+# img = "https://i0.wp.com/nekyou.mangadex.com/wp-content/uploads/sites/83/2019/08/00202.jpg?fit=713%2C1024&ssl=1"
+# img = "https://i.imgur.com/chYG9bU.png"
+# pixiv img:
+# img = "https://i.pinimg.com/originals/61/2f/63/612f63b9efa09cdf65ae3576ca3504b3.jpg"
+# result of interst:
+# img = "https://i.redd.it/jw7vzsuu2nq51.jpg"
+# saucenao doesnt have result
+# img = "https://i.imgur.com/81OEeM8.jpg"
 
 
-def getSauce(url):
-    PATH = "C:\Program Files (x86)\chromedriver.exe"
-    driver = webdriver.Chrome(PATH)
-    driver.get("https://saucenao.com")
-
-    # ActionChains(driver).click(driver.find_element_by_class_name("style7")).perform()
-    expand = driver.find_element_by_link_text("~advanced options~")
-    expand.click()
-    search = driver.find_element_by_name("url")
-    search.send_keys(url)
-    search.send_keys(Keys.RETURN)
-    driver.implicitly_wait(8)
-    # time.sleep(8)
+def GetSauce(url,isSubreddit = False):
+    search = "https://saucenao.com/search.php"
     try:
-        titlename = driver.find_element_by_class_name("resulttitle")
-        print(titlename.text)
-        # print(len(titlename.text))
-        if len(titlename.text) > 0:
-            # To see if a title exists
-
-            titlename = "Sauce: " + str(titlename.text) + "\n\n"
-            # print(len(titlename))
-            content = driver.find_element_by_class_name("resultcontentcolumn")
-            print(content.text)
-            content = str(content.text) + "\n\n"
-            if "pixiv" in content.lower() or "da id" in content.lower() or "seiga" in content.lower() or "nijie" in content.lower():
-                print(driver.find_element_by_css_selector("div.resultcontentcolumn > a").get_attribute('href'))
-                content ="[Sauce link]"+str("(") + str(driver.find_element_by_css_selector("div.resultcontentcolumn > a").get_attribute('href'))+str(")\n\n")
-            similarity = driver.find_element_by_class_name("resultsimilarityinfo")
-            # print("Similarity = " + str(similarity.text))
-            # print(str(similarity.text)[0:2]+" This is a test")
-            similarity = "Similarity = " + str(similarity.text) + "\n" + "\n"
-            footer = "^I ^am ^a ^bot. ^give ^feedback ^to u/GoblinHater ||" + source + " || " + FAQ
-            reply = titlename + content + similarity + footer
-            driver.quit()
-            return reply
-        else:
-            # If title does not exist it means that there was no result
-            
-            print("No result")
-            replyNeg = "Sorry I was not able to find the sauce.\n\nI did not check the low similarity options\n\n"
-            replyNeg = replyNeg + "^I ^am ^a ^bot. ^give ^feedback ^to u/GoblinHater ||" + source + " || " + FAQ
-            driver.quit()
-            return replyNeg
-    except:
-        # If the page has not loaded
-
-        print("No result")
-        replyNeg = "Sorry I was not able to find the sauce.\n\nI did not check the low similarity options\n\n"
-        replyNeg = replyNeg + "^I ^am ^a ^bot. ^give ^feedback ^to u/GoblinHater ||" + source + " || " + FAQ
-        driver.quit()
-        return replyNeg
-
-
-# For giving source on posts on PASSEDSUBREDDIT
-# Slightly modified the function for quality checks and hyperlinks
-def getSauceSubreddit(url):
-    PATH = "C:\Program Files (x86)\chromedriver.exe"
-    driver = webdriver.Chrome(PATH)
-    driver.get("https://saucenao.com")
-
-    # ActionChains(driver).click(driver.find_element_by_class_name("style7")).perform()
-    expand = driver.find_element_by_link_text("~advanced options~")
-    expand.click()
-    search = driver.find_element_by_name("url")
-    search.send_keys(url)
-    search.send_keys(Keys.RETURN)
-    driver.implicitly_wait(8)
-    # time.sleep(8)
-    try:
-        titlename = driver.find_element_by_class_name("resulttitle")
-        print(titlename.text)
-        # print(len(titlename.text))
-        if len(titlename.text) > 0:
-            # To see if a title exists
-
-            titlename = "Sauce: " + str(titlename.text) + "\n\n"
-            # print(len(titlename))
-            content = driver.find_element_by_class_name("resultcontentcolumn")
-            print(content.text)
-            content = str(content.text) + "\n\n"
-
-            # To get links to artwork
-            try:
-                if "pixiv" in content.lower() or "da id" in content.lower() or "seiga" in content.lower() or "nijie" in content.lower():
-                    print(driver.find_element_by_css_selector("div.resultcontentcolumn > a").get_attribute('href'))
-                    content = "[Sauce link]" + str("(") + str(driver.find_element_by_css_selector("div.resultcontentcolumn > a").get_attribute('href')) + str(")\n\n")
-            except:
-                pass
-
-            similarity = driver.find_element_by_class_name("resultsimilarityinfo")
-            # print("Similarity = " + str(similarity.text))
-
-            # Check if similarity is more than 60%
-            if int(str(similarity.text)[0:2]) >= 60:
-                print("Similarity>60")
-                similarity = "Similarity = " + str(similarity.text) + "\n" + "\n"
-                footer = "^I ^am ^a ^bot. ^give ^feedback ^to u/GoblinHater || [Source](https://github.com/GoblinHater/sauceNAOcheckbot) || [FAQ](https://www.reddit.com/r/sauceNAOcheckbot/comments/iocsj9/saucenaocheckbot_faq/)"
-                reply = titlename + content + similarity + footer
-                driver.quit()
-                return reply
+        r = requests.post(search, data={'url': url}, timeout=5)
+        soup = BeautifulSoup(r.content, 'html.parser')
+    # Get all titles that sauceNAO displays
+        all_titles = soup.find_all('div', class_='result')
+        if all_titles:
+            first_title = all_titles[0]
+            first_match = first_title.find('div', class_='resultcontent')
+            #print(first_match)
+            # If there are no resuls
+            if first_match is None:
+                if not isSubreddit:
+                    replyNeg = Negative_reply + footer
+                    return replyNeg
+                else:
+                    print("No results found")
+                    return ""
+    
+            title = first_match.find('div', class_='resulttitle')
+            titletp = str(title.get_text(separator=" ").strip())
+            titletp.replace("  ", " ")
+            titletp += "\n\n"
+            print("titletp: "+titletp)
+    
+            content = first_match.find('div', class_='resultcontentcolumn')
+            fullcontnt = content.get_text(separator=" ").strip()
+            print("full contnt: ", end="")
+            fullcontnt.replace("  ", " ")
+            fullcontnt += "\n\n"
+            print(fullcontnt)
+    
+            similar = soup.find('div', class_="resultsimilarityinfo")
+    # Now 60% requirement for mentions is also 60%
+            similarity_i = float(similar.text[0:-1])
+            if similarity_i < 60 and isSubreddit:
+                print("Similarity of the result was less than 60")
+                return ""
+    
+            print("similarity of first result:", similarity_i)
+    
+            saucelink = ""
+            # Loop through all the results
+            for post in all_titles:
+                subpost_similarity = soup.find('div', class_='resultsimilarityinfo')
+                subpost_similarity = float(subpost_similarity.text[0:-1])
+                # Quality check for 2nd result onwards
+                if subpost_similarity > 70 and abs(subpost_similarity - similarity_i) < 25:
+                    content_check = post.find('div', class_='resultcontentcolumn')
+                    if content_check is not None:
+                        if "pixiv" in content_check.text.lower() or "da" in content_check.text.lower() or "seiga" in content_check.text.lower():
+                            if content_check.find('a'):
+                                for link in content_check.find_all('a'):
+                                    print(link.get('href'))
+                                    saucelink = link.get('href')
+                                    break
+                    if saucelink != "":
+                        similarity_i = subpost_similarity
+                        break
+    
+                    misc_check = post.find('div', class_='resultmiscinfo')
+                    # For GelBooru/DanBooru etc. type of links
+                    if misc_check is not None:
+                        if misc_check.find('a'):
+                            for link in misc_check.find_all('a'):
+                                print(link.get('href'))
+                                saucelink = link.get('href')
+                                break
+    
+                    if saucelink != "":
+                        similarity_i = subpost_similarity
+                        break
+    
+            print("sauce: " + str(saucelink))
+            if len(saucelink) > 0:
+                replyPos = "**Sauce:** " + titletp + "[Sauce link]" + "(" + saucelink + ")\n\n" + "Similarity = " + str(similarity_i) + "\n\n" + footer
+                return replyPos
             else:
-                print("Low similarity")
-                driver.quit()
-
+                replyPos = "**Sauce:** " + titletp + fullcontnt + "Similarity = " + str(similarity_i) + "\n\n" + footer
+                return replyPos
         else:
-            # If title does not exist it means that there was no result
-            driver.quit()
-            print("No result")
-            pass
-
+            if not isSubreddit:
+                replyNeg = Wrong_file + footer
+                return replyNeg
+            else:
+                print("No result")
+                return ""
     except:
-        # If the page has not loaded
-        driver.quit()
-        print("No result")
+        print("Internet not working")
+        
